918c2528858f40da541a87077d019620006a2e14
andro312 <andaesra312@gmail.com>
2025-08-05 14:46:52 -0500

    realizar taller1

--- conceptos_previos/taller1realizado.py
+++ conceptos_previos/taller1realizado.py
@@ -15,18 +15,29 @@ print(df)
 
 #crear una serie con los nombres y alturas de los estudiantes
 altur = pd.DataFrame(datos_estudiantes,columns=["altura"],index=["Ana", "Carlos", "Daniela", "Eduardo", "Fernanda"])
-#print(altur)
+print(altur)
 #cual es la altura de daniela?
 #usando loc
-#print(f"la altura de daniela es de:{altur.loc["Daniela"]['altura']}")
+print(f"la altura de daniela es de:{altur.loc["Daniela"]['altura']}")
 #usando iloc
-#print(f"la altura de daniela es de:{altur.iloc[2]['altura']}")
+print(f"la altura de daniela es de:{altur.iloc[2]['altura']}")
 #Accede al promedio de calificación de Carlos de 3 formas diferentes
 promedios= pd.DataFrame(df,columns=["promedio"],index=["Carlos"])
-#print(f"el promedio de carlos es:{promedios.loc["Carlos"]["promedio"]}")
+print(f"el promedio de carlos es:{promedios.loc["Carlos"]["promedio"]}")
 #Filtra a los estudiantes con promedio mayor o igual a 4.0
-#df2=df.query("promedio >= 4.0")
-#print(df2)
+df2=df.query("promedio >= 4.0")
+print(df2)
 dfcopy= df.copy()
 dfcopy["mayoredad"]= ["no","si","no","si","si"]
 print(dfcopy)
+dfcopy["añonacer"]= 2025 - dfcopy["edad"]
+print(dfcopy)
+promedios2= df["promedio"]
+print(promedios2)
+promedios2.plot()
+plt.show()
+del dfcopy["peso"]
+print(dfcopy)
+dtf3 = pd.DataFrame(dfcopy, columns=["edad","añonacer"],
+                    index=["Ana", "Carlos", "Daniela", "Eduardo", "Fernanda"])
+print(dtf3)
