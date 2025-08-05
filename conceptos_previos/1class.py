8c80705f8c89a734d9fa1309f08b3d7a9ad6ae74
andro312 <andaesra312@gmail.com>
2025-08-05 08:27:30 -0500

    actualizacion sobre pandas1

--- conceptos_previos/1class.py
+++ conceptos_previos/1class.py
@@ -3,3 +3,8 @@ import pandas as pd
 # creacion de objetos serie 
 s= pd.Series ([2,4,6,8,10]) #propiedad de pandas llamada series referente a list
 print(s) 
+
+# creacion de un objeto serie inicializando con un diccionario de python
+altura = {"santiago": 180,"marcelo": 172,"luis": 174,"alejandra": 163,"Andrés":184}
+s = pd.Series(altura)
+print(s)
---
+++ conceptos_previos/intropandas.md
@@ -0,0 +1,10 @@
+# introduccion a pandas
+python --version / Python 3.13.0
+## creacion de obejtos series pandas
+```
+import pandas as pd #pip install pandas
+# creacion de objetos serie 
+s= pd.Series ([2,4,6,8,10])
+print(s)
+
+``` 
