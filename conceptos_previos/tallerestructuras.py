33310045b17d8b34d6b4b739b0d0e005924f0d55
andro312 <andaesra312@gmail.com>
2025-08-05 14:55:23 -0500

    otro taller

---
+++ conceptos_previos/tallerestructuras.py
@@ -0,0 +1,8 @@
+#determinar si un numero es positivo o negativo
+
+a = int(input("registre su numero",))
+if a >= 0 :
+    print(f"su numero{a} es positivo")
+else:
+    print(f"su numero {a} es negativo")
+    
